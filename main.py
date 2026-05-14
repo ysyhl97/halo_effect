2c6bb3daae002cbb819edb15050863d221689952
ysyhl97 <109932551+ysyhl97@users.noreply.github.com>
2026-05-14 21:25:01 +0800

    Revert "Feature/improve load folder"

--- main.py
+++ main.py
@@ -1,40 +1,5 @@
-import logging.config
-
-import yaml
-
-from filter_excel import search_add_keywords, search_only
-from task import search_task, statistic_task
-
-with open("./config/logging_config.yaml", "r", encoding="utf-8") as f:
-    config_dict = yaml.safe_load(f)
-logging.config.dictConfig(config_dict)
-
-
 def main():
-    while True:
-        print("====功能选择====")
-        print("1.筛选")
-        print("2.筛选（加关键字）")
-        print("3.统计")
-        print("9.退出系统：exit/quit")
-
-        number = input("请输入你选择的编号：").lower()
-
-        match number:
-            case "1":
-                print("开始执行筛选功能")
-                search_task(search_only)
-            case "2":
-                print("开始执行筛选功能(添加关键字)")
-                search_task(search_add_keywords)
-            case "3":
-                print("开始进行统计")
-                statistic_task()
-            case "exit" | "quit":
-                print("正在退出系统....")
-                break
-            case _:
-                print(f"你输入的{number}不对，请重新输入对应的任务编号")
+    print("Hello from halo-effect!")
 
 
 if __name__ == "__main__":
